aae7de5a9540353533977d28cc4eb5ff553947a5
Stephen Raoul Sullivan <stephen@beyondspreadsheets.co.uk>
2024-09-16 17:10:33 +0300

    Add an add operation

--- taskmaster.py
+++ taskmaster.py
@@ -5,7 +5,8 @@ from models import Task
 
 HELLO = "hello"
 LIST = "list"
-CHOICES = [HELLO, LIST]
+ADD = 'add'
+CHOICES = [HELLO, LIST, ADD]
 DATABASE = "taskmaster.sqlite"
 
 def get_tasks():
@@ -16,10 +17,23 @@ def get_tasks():
         session.close()
     return tasks
 
+def add_task(name):
+    session = SessionLocal()
+    task = Task(name=name)
+    try:
+        session.add(task)
+        session.commit()
+    finally:
+        session.close()
+    return task
+   
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='TaskMaster')
     parser.add_argument('command', type=str, choices=CHOICES, 
                         help='The command for TaskMaster to run.')
+    parser.add_argument('-n', '--name', type=str, 
+                        help='The name of the task to add')
     args = parser.parse_args()
     match args.command:
         case 'hello':
@@ -27,5 +41,11 @@ if __name__ == '__main__':
         case 'list':
             for task in get_tasks():
                 print(task['name'])
+        case 'add':
+            name = args.name
+            if not name:
+                raise ValueError("Name cannot be None")
+            add_task(name)
+            print(f'Task {name} added')
         case _:
             raise NotImplementedError("Missing default action")
