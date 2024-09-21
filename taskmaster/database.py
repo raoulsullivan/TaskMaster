0edf1ac9d3fc9175c2cb395e429f015b1aea5aea
Stephen Raoul Sullivan <stephen@beyondspreadsheets.co.uk>
2024-09-21 07:59:16 +0300

    Set PRAGMA foreign_keys to restrict deletes

--- taskmaster/database.py
+++ taskmaster/database.py
@@ -1,11 +1,19 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine import Engine
 
 DATABASE_URL = "sqlite:///./taskmaster.sqlite"  
 
 engine = create_engine(
     DATABASE_URL,
 )
+
+# Listen for the SQLite connection and set PRAGMA foreign_keys=ON
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
