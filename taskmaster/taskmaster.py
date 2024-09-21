f2d67daca7d551569a854a968f6e49760ca6aa63
Stephen Raoul Sullivan <stephen@beyondspreadsheets.co.uk>
2024-09-21 18:28:35 +0300

    Add show command
    This gets details on a specific task, at the moment that's the list of
    executions

--- taskmaster/taskmaster.py
+++ taskmaster/taskmaster.py
@@ -1,6 +1,8 @@
 import click
 from taskmaster.database import SessionLocal
 from taskmaster.models import Task, Execution
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy import desc
 
 HELLO = "hello"
 LIST = "list"
@@ -33,6 +35,23 @@ def list():
         click.echo(f'{task.id} - {task.name}')
 
 cli.add_command(list)
+
+@click.command()
+@click.argument('task_id')
+def show(task_id):
+    session = SessionLocal()
+    try:
+        task = session.query(Task).filter(Task.id == task_id).one()
+        executions = session.query(Execution).filter(Execution.task_id == task.id).order_by(desc(Execution.executed_at)).all()
+        click.echo(f'{task.id} - {task.name}')
+        click.echo()
+        click.echo('Executions:')
+        for execution in executions:
+            click.echo(f'{execution.executed_at.strftime("%Y-%m-%d %H:%M")}')
+    except NoResultFound:
+        click.echo(f'Task with id {task_id} not found')
+
+cli.add_command(show)
 
 @click.command()
 @click.argument('name')
