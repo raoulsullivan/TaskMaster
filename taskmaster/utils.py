5d9671bea92aa9625f2a98eab5f36e506f7741fe
raoulsullivan <stephen@beyondspreadsheets.co.uk>
2024-10-28 15:06:29 +0200

    Lint utils.py

--- taskmaster/utils.py
+++ taskmaster/utils.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 
-def fuzzy_datetime_validator(input, raise_if_invalid = True):
+
+def fuzzy_datetime_validator(input, raise_if_invalid=True):
     """
-    This helper function will attempt to coerce an input into a datetime. Accepted input formats are:
+    This helper function will attempt to coerce an input into a datetime.
+    Accepted input formats are:
     * '%Y-%m-%d %H:%M'
     * '%Y-%m-%d' (will set 00:00 as time)
     * '%m-%d' (will set current year and 00:00 as time)
@@ -28,6 +30,7 @@ def fuzzy_datetime_validator(input, raise_if_invalid = True):
         pass
 
     if not date and raise_if_invalid:
-        raise ValueError(f'Provided input {input} cannot be coerced to a datetime')
+        raise ValueError(
+            f'Provided input {input} cannot be coerced to a datetime')
 
     return date
