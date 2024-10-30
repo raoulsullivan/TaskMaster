86f6a2a514a59a7f7184852f8c173434c1f1704f
raoulsullivan <stephen@beyondspreadsheets.co.uk>
2024-10-30 12:27:21 +0200

    Use Jinja2 templating to create response

--- website/app.py
+++ website/app.py
@@ -1,6 +1,15 @@
+from jinja2 import Environment, FileSystemLoader
+
+env = Environment(loader=FileSystemLoader('templates'))
+
+
 def app(environ, start_response):
-    response_body = b'Hello, World, this is TaskMaster!'
+    template = env.get_template('hello.html')
+    content = 'Hello, World, this is TaskMaster!'
+    data = {'content': content}
+    html_output = template.render(data)
+    byte_output = html_output.encode("utf-8")
     status = '200 OK'
-    headers = [('Content-Type', 'text/plain')]
+    headers = [('Content-Type', 'text/html; charset=utf-8')]
     start_response(status, headers)
-    return [response_body]
+    return [byte_output]
